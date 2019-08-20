21aab0751820dfc842335ea62897af923bbfe69c
Mike North <michael.l.north@gmail.com>
2019-08-19 21:26:33 -0700

    initial commit

---
+++ src/middleware/apiDocs.ts
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import * as swaggerUi from "swagger-ui-express";
+import * as swaggerDocument from "../config/swagger.json";
+
+export const handleAPIDocs = (router: Router) =>
+  router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
