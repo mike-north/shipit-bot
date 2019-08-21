75a4610d0cfe0ddab97b0eda0d19fdb7f71a0632
Mike North <michael.l.north@gmail.com>
2019-08-20 19:27:07 -0700

    credentials

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,31 @@
-import { Application } from 'probot' // eslint-disable-line no-unused-vars
+import { Application } from "probot"; // eslint-disable-line no-unused-vars
 
 export = (app: Application) => {
-  app.on('issues.opened', async (context) => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    await context.github.issues.createComment(issueComment)
-  })
+  app.on(`*`, async context => {
+    context.log({ event: context.event, action: context.payload.action });
+  });
+
+  app.on("issues.opened", async context => {
+    const issueComment = context.issue({
+      body: "Thanks for opening this issue!"
+    });
+    await context.github.issues.createComment(issueComment);
+  });
+  app.on("issues.reopened", async context => {
+    const issueComment = context.issue({
+      body: "Thanks for reopening this issue!"
+    });
+    await context.github.issues.createComment(issueComment);
+  });
+  app.on("issues.closed", async context => {
+    const issueComment = context.issue({
+      body: "Thanks for closing this issue!"
+    });
+    await context.github.issues.createComment(issueComment);
+  });
   // For more information on building apps:
   // https://probot.github.io/docs/
 
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
-}
+};
