77dbcec5e12a12a18f4c56781914e05fccab2acf
Mike North <michael.l.north@gmail.com>
2019-08-26 09:46:48 -0700

    debounce event handlers
    prior to this change, event handlers that subscribed to _multiple_ events
    would fire _multiple times_. This was not desirable.

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import { Application } from 'probot'; // eslint-disable-line no-unused-vars
 import { updateAclStatus } from './event-handlers/pr/acl';
 import { debounce } from './utils/debounce';
 
+const STANDARD_DEBOUNCE = 100; // ms
+
 export = (app: Application): void => {
   app.on(`*`, async context => {
     context.log({ event: context.event, action: context.payload.action });
@@ -9,6 +11,6 @@ export = (app: Application): void => {
 
   app.on(
     ['pull_request', 'pull_request.edited'],
-    debounce(updateAclStatus, 100),
+    debounce(updateAclStatus, STANDARD_DEBOUNCE),
   );
 };
