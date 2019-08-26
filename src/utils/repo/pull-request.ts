4ed02c0a73fb39771e160fcb74483a80b88d56ee
Mike North <michael.l.north@gmail.com>
2019-08-26 09:50:26 -0700

    get list of SHAs relating to a PR

---
+++ src/utils/repo/pull-request.ts
@@ -0,0 +1,23 @@
+import { GitHubAPI } from "probot/lib/github";
+
+/**
+ * Get the list of commit SHAs that relate to a specified pull request
+ *
+ * @param github GitHub api
+ * @param owner repo owner
+ * @param repo repo name
+ * @param pull_number pull request number
+ */
+export async function getCommitHistoryForPullRequest(
+  github: GitHubAPI,
+  owner: string,
+  repo: string,
+  pull_number: number
+): Promise<string[]> {
+  const { data: commitList } = await github.pulls.listCommits({
+    pull_number,
+    repo,
+    owner
+  });
+  return commitList.map(c => c.sha);
+}
